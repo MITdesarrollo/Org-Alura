c9c57bf977729c367d4903e167a3a67ed9d949f0
mariel <marieltorres97@gmail.com>
2023-09-04 18:54:45 -0300

    fix icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,11 +4,11 @@ export const Footer = () => {
     return (
         <footer className='footer'>
             <div className='links'>
-                <a href=""><img src="src/assets/linkedin.png" alt=''/></a>
-                <a href=""><img src="src/assets/github.png" alt=''/></a>
-                <a href=""><img src="src/assets/behance.png" alt=''/></a>
+                <a href=""><img src="public/linkedin.png" alt=''/></a>
+                <a href=""><img src="public/github.png" alt=''/></a>
+                <a href=""><img src="public/behance.png" alt=''/></a>
             </div>
-            <img className='logo' src='src/assets/Logo.png' alt="" />
+            <img className='logo' src='public/Logo.png' alt="" />
             <h3>Desarrollado por Mariel Torres</h3>
         </footer>
     )
