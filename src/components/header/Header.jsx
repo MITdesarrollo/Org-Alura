898623292250a90c9f9bac497cbbb8ae4d3fe969
mariel <marieltorres97@gmail.com>
2023-09-04 19:53:57 -0300

    fix icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 //  import Banner from 'public/img/header.png';
 import "./Header.css"
-import Banner from "./Header.png"
+
 
 export const Header = () => {
     return (
         <header className='header'> 
-            <img src={ Banner }alt="logo"/>
+            <img src="./Header.png" alt="logo"/>
         </header>       
     )
 }
